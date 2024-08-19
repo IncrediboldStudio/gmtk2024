53bdb2aaf69d7a916e0a7d6478d3a0c83cd88feb
Gabnixe <gabrielstlaurentrecoura@gmail.com>
2024-08-18 21:32:15 -0400

    Block preview in palette

--- src/ui/components/hud/blocks_palette/block_type_button/block_type_button.gd
+++ src/ui/components/hud/blocks_palette/block_type_button/block_type_button.gd
@@ -2,6 +2,11 @@ extends Control
 
 @export var block_data : BlockData
 
+func _ready():
+    var text_rect = get_node("MarginContainer/TextureRect")
+    text_rect.sprite_frames = block_data.sprite_frames
+    text_rect.animation = "preview"
+
 func _on_block_selected():
     EventEngine.change_selected_block.emit(block_data)
     SfxManager.play_sfx(SfxManager.SfxName.UI_POP_3, SfxManager.SfxVariation.MEDIUM)
--- src/utils/animated_texture_rect.gd
+++ src/utils/animated_texture_rect.gd
@@ -1,6 +1,9 @@
 extends TextureRect
 
+class_name AnimatedTextureRect
+
 @export var sprite_frames : SpriteFrames : set = _set_sprite_frames
+@export var animation = "default" : set = _set_animation
 
 var frame : int : set = _set_frame
 var frame_count : int
@@ -9,21 +12,23 @@ var time_since_last_frame : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_set_sprite_frames(sprite_frames)
+	if sprite_frames:
+		_set_sprite_frames(sprite_frames)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
-	time_since_last_frame = time_since_last_frame + delta
-	var nb_frames_till_last_update = time_since_last_frame / frame_time
-	if nb_frames_till_last_update > 1:
-		frame = frame + int(nb_frames_till_last_update)
-		time_since_last_frame = time_since_last_frame - (nb_frames_till_last_update * frame_time)
+	if sprite_frames:
+		time_since_last_frame = time_since_last_frame + delta
+		var nb_frames_till_last_update = time_since_last_frame / frame_time
+		if nb_frames_till_last_update > 1:
+			frame = frame + int(nb_frames_till_last_update)
+			time_since_last_frame = time_since_last_frame - (nb_frames_till_last_update * frame_time)
 
 func _set_sprite_frames(value : SpriteFrames):
 	sprite_frames = value
-	frame_count = sprite_frames.get_frame_count("default")
-	frame_time = 1 / sprite_frames.get_animation_speed("default")
+	frame_count = sprite_frames.get_frame_count(animation)
+	frame_time = 1 / sprite_frames.get_animation_speed(animation)
 	time_since_last_frame = 0
 	frame = 0
 
@@ -31,4 +36,9 @@ func _set_frame(value : int):
 	frame = value
 	if frame >= frame_count:
 		frame = frame - frame_count
-	texture = sprite_frames.get_frame_texture("default", frame)
+	texture = sprite_frames.get_frame_texture(animation, frame)
+
+func _set_animation(value : String):
+	animation = value
+	#Reset Frame since we changed animation
+	sprite_frames = sprite_frames
